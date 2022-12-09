a6889c66bb0743963e8f0b1011143d63d489c044
youyupei <youyupei@gmail.com>
2022-12-09 07:37:46 +0000

    start to run in multiprocess

--- bin/perfect_read_generator.py
+++ bin/perfect_read_generator.py
@@ -6,6 +6,13 @@ from tqdm import tqdm
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from random import randint
+from datetime import datetime
+import os
+import shutil
+import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import multiprocessing as mp
+from multiprocessing import RLock
 
 import helper
 import config
@@ -33,6 +40,16 @@ def parse_arg():
                         '''
                         ))
     parser.add_argument('-o','--output-filename',  type=str, default='template.fa',
+                        help=textwrap.dedent('''
+                        Filename of simulated reads.
+                        '''
+                        ))
+    parser.add_argument('--mRNA-len',  type=int, default=200,
+                        help=textwrap.dedent('''
+                        Filename of simulated reads.
+                        '''
+                        ))
+    parser.add_argument('--thread',  type=int, default=1,
                         help=textwrap.dedent('''
                         Filename of simulated reads.
                         '''
@@ -67,7 +84,8 @@ def artificial_template(BC_list,
                         adaptor,
                         TSO,
                         output_fn,
-                        amp_rate = None
+                        amp_rate = None,
+                        pbar_pos = 1
                         ):
     '''
     Generate a barcode 
@@ -84,14 +102,19 @@ def artificial_template(BC_list,
     ''' 
     if repeats is None:
         repeats = [1]*len(BC_list)
-        
+    
+    lock = tqdm.get_lock()
+
     # v3 adaptor
     with open(output_fn, 'a') as f:
         random_frag = get_random_frag(tran_ref, tran_len)
         read_index = 0
-        pbar = tqdm(total=sum(repeats), desc='Total UMI count')
-        for BC,count in tqdm(zip(BC_list, repeats), total = len(BC_list), desc='BCs'):
-            for i in tqdm(range(count), leave=False, desc = f'Reads for BC: {BC}'):
+        
+        #pbar = tqdm(total=sum(repeats), desc='Total UMI count')
+
+        pbar = tqdm(total=sum(repeats), desc=f'Thread #{pbar_pos}: Total UMI count', position = pbar_pos, leave=False )
+        for BC,count in zip(BC_list, repeats):
+            for i in range(count):
                 seq = adaptor+BC+random_seq(12) + polyT(15) + next(random_frag) +TSO
                 
                 if amp_rate is None:
@@ -117,8 +140,14 @@ def artificial_template(BC_list,
                             f.write(seq + '\n')
                         else:
                             f.write(helper.reverse_complement(seq) + '\n')
+                
+                
+                lock.acquire()
                 pbar.update(1)
-                    
+                lock.release()
+                
+
+
 def get_random_frag(ref, frag_len):                    
     # There should be one and only one record, the entire genome:
     records = list(SeqIO.parse(ref, "fasta"))
@@ -140,17 +169,65 @@ def get_random_frag(ref, frag_len):
             if 'N' in frag:
                 continue
             yield helper.reverse_complement(frag)
-                            
+
+def chunk_generator(df, num_chunk):
+    '''split umi count dataframe into chunks with simular UMI count
+    '''
+    total_umi = df.counts.sum()
+    
+    umi_per_chunk = total_umi/num_chunk
+    current_count = 0
+    current_idx = 0
+    for idx, i in enumerate(df.counts):
+        current_count += i
+        if current_count > umi_per_chunk:
+            yield df.loc[current_idx: idx]
+            current_idx = idx+1
+            current_count = 0
+    yield df.loc[current_idx:]
+
 def main():
     args = parse_arg()
     BC_df = pd.read_csv(args.umi_count_file)
-    artificial_template(BC_df.BC, 
-                        BC_df.counts, 
-                        tran_len=200,
-                        tran_ref=args.trans_ref,
-                        adaptor=config.ADPTER_SEQ,
-                        TSO=config.TSO_SEQ,
-                        output_fn=args.output_filename,
-                        amp_rate=args.amp_rate)
+    total_umi = BC_df.counts.sum()
+    tqdm.set_lock(RLock())
+    if args.thread == 1:
+        artificial_template(BC_list=BC_df.BC,
+                            repeats=BC_df.counts,
+                            tran_len=args.mRNA_len,
+                            tran_ref=args.trans_ref,
+                            adaptor=config.ADPTER_SEQ,
+                            TSO=config.TSO_SEQ,
+                            output_fn=args.output_filename,
+                            amp_rate=args.amp_rate)
+    
+    else: # multiprocessing
+        # create tmp dir
+        tmp_dirname = f".tmp_{datetime.now()}"
+        os.mkdir(tmp_dirname)
+ 
+        chunks = chunk_generator(BC_df, args.thread)
+        executor = concurrent.futures.ProcessPoolExecutor(args.thread)
+        futures = {}
+
+        for idx, chunk in enumerate(chunks):
+            futures[executor.submit(artificial_template,
+                            BC_list=chunk.BC,
+                            repeats=chunk.counts,
+                            tran_len=args.mRNA_len,
+                            tran_ref=args.trans_ref,
+                            adaptor=config.ADPTER_SEQ,
+                            TSO=config.TSO_SEQ,
+                            output_fn=f'{tmp_dirname}/tmp_{idx}',
+                            amp_rate=args.amp_rate,
+                            pbar_pos = idx+1)] = None
+        concurrent.futures.wait(futures)
+        for fn in os.listdir(tmp_dirname):
+            with open(fn, 'rb') as readfile:
+                shutil.copyfileobj(f'{tmp_dirname}/{fn}', args.output_filename)
+        
+        # clean tmp dir
+        shutil.rmtree(tmp_dirname)
+
 if __name__=='__main__':
     main()
